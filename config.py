c6656dd19f52ef039d1d3e1d6eef2793dc8bc795
Abhinandan <a23894843@gmail.com>
2026-08-04 19:16:18 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,1 +1,20 @@
+import os
 
+# Base Paths
+BASE_DIR = os.path.dirname (os.path.abspath (__file__))
+ENGINE_DIR = os.path.join(BASE_DIR, "engine")
+SENSOR_DIR = os.path.join(BASE_DIR, "sensor")
+
+#IPC Socket Paths
+UDS_MOUSE = os.path.join(BASE_DIR, "Zero_Context_mouse.sock")
+UDS_KBD = os.path.join(BASE_DIR, "Zero_Context_kbd.sock")
+
+# Security Thresholds
+VELOCITY_CEILING = 15000   #px/s
+
+# C++ Daemon Paths
+SENSOR_BIN = os.path.join(SENSOR_DIR, "sensor_daemon")
+SENSOR_SRC = os.path.join(SENSOR_DIR, "sensor_daemon.cpp")
+
+# Persistance  Logging
+TELEMETRY_LOG = os.path.join(BASE_DIR, "zero_context_telemetry.jsonl")
--- main.py
+++ main.py
@@ -1,1 +1,58 @@
+import os
+import sys
+import json
+import math
+import time
+import asyncio
+import socket
+import subprocess
+import config
+from engine.core import ZeroContextEngine
 
+async def main():
+    # 1. Auto-Compile the C++ daemon if needed (Blocking is fine here)
+    if not os.path.exists(config.SENSOR_BIN):
+        print("[*] Compiling Multi-Threaded C++ Daemon...")
+        try:
+            subprocess.run(["g++", "-pthread", config.SENSOR_SRC, "-o", config.SENSOR_BIN], check=True)
+            print("[*] Compilation successful.")
+        except subprocess.CalledProcessError:
+            print("[!] FATAL: Compilation failed.")
+            exit(1)
+
+    # 2. Launch the C++ daemon in the background (Non-Blocking)
+    print("[*] Launching C++ Sensor Daemon with root privileges...")
+    print("[*] Note: You may be prompted for your sudo password.")
+    sensor_process = subprocess.Popen(["sudo", config.SENSOR_BIN])
+
+    # 3. Start the Python UDS Server
+    for sock in [config.UDS_MOUSE, config.UDS_KBD] :
+            if os.path.exists(sock):
+                os.remove(sock)
+        
+    engine = ZeroContextEngine(config)
+    mouse_server = await asyncio.start_unix_server(engine.handle_mouse, path = config.UDS_MOUSE)
+    kbd_server = await asyncio.start_unix_server(engine.handle_keyboard, path = config.UDS_KBD)
+    
+    print(f"[*] ZeroContext Backend listening on {config.UDS_MOUSE}")
+    
+    try:
+        async with mouse_server, kbd_server:
+            await asyncio.gather(mouse_server.serve_forever(), kbd_server.serve_forever())
+    finally:
+        # 4. Clean up the C++ process if the Python server shuts down
+        print("\n[*] Shutting down C++ Sensor Daemon...")
+        sensor_process.terminate()
+
+if __name__ == "__main__" :
+    try:
+        os.remove(os.path.join(config.BASE_DIR, "zero_context_telemetry.jsonl"))
+    except FileNotFoundError:
+        pass
+    except OSError as e:
+        print(f"Failed to delete telemetry file: {e}")
+
+    try :
+        asyncio.run(main())
+    except KeyboardInterrupt :
+        print("\n[*] ZeroContext System Halted.")
